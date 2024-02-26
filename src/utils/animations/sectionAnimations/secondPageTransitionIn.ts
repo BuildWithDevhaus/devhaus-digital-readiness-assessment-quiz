7f0c3ce9751dbf9a896486e0d494bc55b691316c
Barjuan Davis <davis@devhaus.com.sg>
2024-02-26 13:49:21 +0700

    fixed an issue where buttons are not detected

--- src/utils/animations/sectionAnimations/secondPageTransitionIn.ts
+++ src/utils/animations/sectionAnimations/secondPageTransitionIn.ts
@@ -5,7 +5,7 @@ export default function secondPageTransitionIn(elem: HTMLElement, duration = 100
   const tagline = elem.querySelector('#second-page-tagline') as HTMLElement;
   const title = elem.querySelector('#second-page-title') as HTMLElement;
   const subtitle = elem.querySelector('#second-page-content') as HTMLElement;
-  const button = elem.querySelector('.button-regular') as HTMLElement;
+  const button = elem.querySelector('#second-page-button') as HTMLElement;
 
   const tl = anime({
     targets: [tagline, title, subtitle, button],
